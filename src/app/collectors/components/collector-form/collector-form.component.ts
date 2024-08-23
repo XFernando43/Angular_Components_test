5db6b5464b7847503271932b5a227e444b6555ba
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-08-23 12:02:39 -0500

    add: form for collectors

--- src/app/collectors/components/collector-form/collector-form.component.ts
+++ src/app/collectors/components/collector-form/collector-form.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-collector-form',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule],
   templateUrl: './collector-form.component.html',
   styleUrl: './collector-form.component.css'
 })
