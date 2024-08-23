d01890e0b5fc47c46c73c9e6c08f9f530ab380f9
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-08-23 11:06:36 -0500

    login view added

--- src/app/collectors/pages/login-page/login-page.component.ts
+++ src/app/collectors/pages/login-page/login-page.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
+//
 
+import { LoginFormComponent } from '../../components/login-form/login-form.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule,MatInputModule],
+  imports: [FormsModule, MatFormFieldModule,MatInputModule,LoginFormComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
