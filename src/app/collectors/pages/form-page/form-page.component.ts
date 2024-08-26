86bc7ca9b9cf79e751c155e70c5ce2ba459cd593
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-08-26 09:39:41 -0500

    fix:form added

--- src/app/collectors/pages/form-page/form-page.component.ts
+++ src/app/collectors/pages/form-page/form-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CollectorFormComponent } from '../../components/collector-form/collector-form.component';
+
 @Component({
   selector: 'app-form-page',
   standalone: true,
