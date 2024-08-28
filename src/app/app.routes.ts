154a369ca14caadc16b45db1062b0ed2a2dbfb69
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-08-28 15:11:38 -0500

    add: routing not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,19 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './collectors/pages/login-page/login-page.component';
 import { FormPageComponent } from './collectors/pages/form-page/form-page.component';
 import { DashboardPageComponent } from './collectors/pages/dashboard-page/dashboard-page.component';
+import { NotFoundPageComponent } from './public/pages/not-found-page/not-found-page.component';
 
 export const routes: Routes = [
-
-    {
-        path:'Login',
-        component:LoginPageComponent
-    },
-    {
-        path:'Dashboard',
-        component:DashboardPageComponent
-    },
-    {
-        path:'Form',
-        component:FormPageComponent
-    }
+    { path: '', redirectTo: 'Login', pathMatch: 'full' },
+    {path:'Login', component:LoginPageComponent},
+    {path:'Dashboard',component:DashboardPageComponent},
+    {path:'Form',component:FormPageComponent},
+    {path:'**', component:NotFoundPageComponent}
 ];
