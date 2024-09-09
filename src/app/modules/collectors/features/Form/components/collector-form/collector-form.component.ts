d7fbbbadf1814d4b8879ddadab228faf6f2ffbd7
Fernando Aquino <102422211+XFernando43@users.noreply.github.com>
2024-09-09 18:11:54 -0500

    add: datepikcer customize

--- src/app/modules/collectors/features/Form/components/collector-form/collector-form.component.ts
+++ src/app/modules/collectors/features/Form/components/collector-form/collector-form.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy ,Component } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { InputFilesComponent } from '../input-files/input-files.component';
 import { CurrencyFormat } from '../../../../../../shared/directives/currency-format.directive';
 
+// import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+// import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+
 @Component({
   selector: 'app-collector-form',
   standalone: true,
-  imports: [MatFormFieldModule,InputFilesComponent,CurrencyFormat],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatFormFieldModule,InputFilesComponent,CurrencyFormat,MatInputModule, MatDatepickerModule],
   templateUrl: './collector-form.component.html',
-  styleUrl: './collector-form.component.css'
+  styleUrl: './collector-form.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CollectorFormComponent {
-
+  readonly startDate = new Date(1990, 0, 1);
 }
